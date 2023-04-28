c59f0f387882896aa7a36d67b94d39aeed535757
ahoogthomsen <ahoogthomsen@gmail.com>
2023-04-28 11:32:40 +0200

    initiated project, added movies, list item and grid list components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  exportPathMap: () => {
+    return {
+      "/": { page: "/movies" },
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
