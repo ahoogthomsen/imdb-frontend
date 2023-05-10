8e7a3462be84c3a2347deb87410cc61cc2861976
ahoogthomsen <ahoogthomsen@gmail.com>
2023-05-10 16:49:55 +0200

    added fetch implementation to characters

--- pages/fetch-example/index.js
+++ pages/fetch-example/index.js
@@ -1,27 +1,89 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Characters.module.css";
 import Toaster from "./components/Toaster/Toaster";
 import CharacterListItem from "./components/CharacterListItem/CharacterListItem";
 import CharacterModal from "./components/CharacterModal/CharacterModal";
-import { charactersData } from "./charactersData";
 
 export default function Characters() {
-  const [data, setData] = useState(charactersData);
+  const [data, setData] = useState([]);
   const [toaster, setToaster] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/characters?apiKey=5", {
+          method: "GET",
+        });
+
+        const data = await res.json();
+        setData(data);
+      } catch (e) {
+        setToaster({
+          message: "Ops! Något gick fel",
+          type: "error",
+        });
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const closeToaster = () => {
     setToaster(null);
   };
 
   const handleDelete = async (id) => {
-    setData((prevState) => prevState.filter((char) => char.id !== id));
+    try {
+      const res = await fetch(
+        `http://localhost:3001/characters/${id}?apiKey=5`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      const data = await res.json();
+      if (res.status !== 200) {
+        setToaster({
+          message: data.message,
+          type: "error",
+        });
+        return;
+      }
+
+      setData((prevState) => prevState.filter((char) => char.id !== id));
+    } catch (e) {
+      setToaster({
+        message: "Datan kunde inte deletas",
+        type: "error",
+      });
+    }
   };
 
   const handleAddCharacter = async (name) => {
-    const randomId = Math.floor(Math.random() * 100);
-    const newData = { id: randomId, name };
-    setData((prevState) => [...prevState, newData]);
+    try {
+      const res = await fetch("http://localhost:3001/characters?apiKey=5", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ character: { name } }),
+      });
+
+      const data = await res.json();
+
+      if (res.status !== 200) {
+        setToaster({ message: data.message, type: "error" });
+        return;
+      }
+
+      setData((prevState) => [...prevState, data]);
+    } catch (e) {
+      setToaster({
+        message: "An error occured when trying to add a character",
+        type: "error",
+      });
+    }
   };
 
   const handleToggleModal = () => {
@@ -29,12 +91,37 @@ export default function Characters() {
   };
 
   const handleEditCharacter = async ({ id, name }) => {
-    const editedData = { id, name };
-    setData((prevState) =>
-      prevState.map((char) =>
-        char.id === id ? { ...char, ...editedData } : char
-      )
-    );
+    try {
+      const res = await fetch(
+        `http://localhost:3001/characters/${id}?apiKey=5`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ character: { id, name } }),
+        }
+      );
+
+      const data = await res.json();
+
+      if (res.status !== 200) {
+        setToaster({
+          message: data.message,
+          type: "error",
+        });
+        return;
+      }
+
+      setData((prevState) =>
+        prevState.map((char) => (char.id === id ? { ...char, ...data } : char))
+      );
+    } catch (e) {
+      setToaster({
+        message: "An error occured when trying to edit a character",
+        type: "error",
+      });
+    }
   };
 
   return (
